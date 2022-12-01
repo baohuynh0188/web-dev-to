76b9cf55c19a5cf716fc0600a2ca7f10627764cb
Bao Huynh <baohuynh0188@outlook.com>
2022-12-01 23:22:33 +0700

    init app

---
+++ src/main/java/com/microservice/web/DevToApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToApplication.class, args);
+	}
+
+}
